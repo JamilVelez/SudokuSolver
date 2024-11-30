b465c72acbe7a44b321438c26ff84c9d93c91711
JamilVelez <60303573+JamilVelez@users.noreply.github.com>
2024-11-30 11:59:29 -0500

    Added function to read the SudokuPuzzles.txt
    Reads the content of SudokuPuzzles.txt and prints it to confirm it works (print can be removed later)

--- Program3 Sudoku Solving/src/Main.java
+++ Program3 Sudoku Solving/src/Main.java
@@ -8,7 +8,20 @@ Program that will solve sudoku puzzles fed into it using Depth First Search and/
 import java.util.*;
 import java.io.*;
 
+//BFS: https://stackoverflow.com/questions/5262308/how-do-implement-a-breadth-first-traversal
+//DLS: https://tutorialedge.net/artificial-intelligence/depth-limited-search-in-java/
 public class Main {
-
-
+    public static void main(String[] args) {
+        String SudokuFile = "Program3 Sudoku Solving/src/SudokuPuzzles.txt";
+        try (BufferedReader br = new BufferedReader(new FileReader(SudokuFile))){
+            String line;
+            System.out.println("Sudoku puzzles:\n");
+            while ((line = br.readLine()) != null){
+                System.out.println(line);
+            }
+        }
+        catch (IOException e){
+            System.err.println("Error reading file " + e.getMessage());
+        }
+    }
 }
